2a322983341dd0b0fa4f998c806761e67a8832a0
GonPerez <gperez@estudiantes.unsam.edu.ar>
2021-11-12 21:40:21 -0300

    finish

--- __init__.py
+++ __init__.py
@@ -11,62 +11,71 @@ with app.app_context():
     db.create_all()
     db.session.commit()
 
-@app.route("/",methods=['GET'])
+@app.route('/')
+def routeador():
+    return redirect(url_for('home'))
+
+
+@app.route("/contenido",methods=['GET'])
 def home():
     return render_template("home.html")
 
-@app.route("/<int:id_comentario>/borrarComentario",methods=['GET','DELETE'])
-def borrarComentario(id_comentario):
+
+@app.route("/contenido/<int:_id_contenido>/comentario/",methods=['GET'])
+def obtenerComentario(_id_contenido):
+    comentarios= Comentario.query.filter_by(id_contenido=_id_contenido)
+    print(comentarios)
+    return render_template('mostrarComentarios.html',comentarios=comentarios, id = _id_contenido)
+
+
+@app.route("/contenido/<int:_id_contenido>/comentario/<int:id_comentario>/delete",methods=['GET','DELETE'])
+def borrarComentario(_id_contenido, id_comentario):
     comentario= Comentario.query.filter_by(id=id_comentario).first()
-    id_contenidop=comentario.id_contenido
     db.session.delete(comentario)
     db.session.commit()
-    return redirect(url_for('obtenerComentario', _id_contenido = id_contenidop))
+    return redirect(url_for('obtenerComentario', _id_contenido = _id_contenido))
 
-@app.route("/<int:id_comentario>/editarComentario",methods=['GET','POST'])
-def editarComentario(id_comentario):
+
+@app.route("/contenido/<int:_id_contenido>/comentario/<int:id_comentario>/update",methods=['GET','POST'])
+def editarComentario(_id_contenido, id_comentario):
     _comentario= Comentario.query.filter_by(id=id_comentario).first()
     if request.method == 'POST':
         _comentario.titulo= request.form.get('titulo')
         _comentario.descripcion = request.form.get('descripcion')
         db.session.add(_comentario)
         db.session.commit()
-        return redirect(url_for('obtenerComentario', _id_contenido = _comentario.id_contenido))
-    return render_template('editarComentario.html',comentario=_comentario, id = id_comentario)
+        return redirect(url_for('obtenerComentario', _id_contenido = _id_contenido))
+    return render_template('editarComentario.html', comentario=_comentario)
 
-@app.route("/<int:id_contenido>/nuevoComentario",methods=['GET','POST'])
-def comentario(id_contenido):
+
+@app.route("/contenido/<int:_id_contenido>/comentario/create",methods=['GET','POST'])
+def crearComentario(_id_contenido):
     if request.method == 'POST':
         apodo = request.form.get('apodo')
         titulo= request.form.get('titulo')
         detalle = request.form.get('descripcion')
-        newComment = Comentario(apodo,titulo,detalle,id_contenido)  
+        newComment = Comentario(apodo,titulo,detalle,_id_contenido)  
         db.session.add(newComment)
         db.session.commit()
-        return redirect(url_for('obtenerComentario', _id_contenido = id_contenido))
-    return render_template('nuevoComentario.html', id = id_contenido)
+        return redirect(url_for('obtenerComentario', _id_contenido = _id_contenido))
+    return render_template('nuevoComentario.html', id = _id_contenido)
 
-@app.route("/<int:_id_contenido>/obtenerComentario",methods=['GET'])
-def obtenerComentario(_id_contenido):
-    comentarios= Comentario.query.filter_by(id_contenido=_id_contenido)
-    print(comentarios)
-    return render_template('mostrarComentarios.html',comentarios=comentarios, id = _id_contenido)
 
 #las replicas de las replicas se distinguen con id_comentairio=0
-@app.route("/<int:_id_contenido>/obtenerComentario/<int:_id_comentario>/replicas",methods=['GET'])
-def obtenerReplicas(_id_contenido, _id_comentario):
-    replicas= Replica.query.filter_by(id_comentario=_id_comentario)
-    #func() 
-    com = Comentario.query.filter_by(id=_id_comentario).first()
-    return render_template('replicas.html',replicas=replicas, id_contenido=_id_contenido, comentario = com.titulo)
+@app.route("/contenido/<int:_id_contenido>/comentario/<int:id_comentario>/replica",methods=['GET'])
+def obtenerReplicas(_id_contenido, id_comentario):
+    replicas= Replica.query.filter_by(id_comentario=id_comentario)
+    com = Comentario.query.filter_by(id=id_comentario).first()
+    return render_template('replicas.html',replicas=replicas, id_contenido=_id_contenido, comentario = com)
 
-@app.route("/<int:_id_contenido>/obtenerComentario/<int:_id_comentario>/replicas/<int:_id_replica>",methods=['GET'])
-def obtenerReplicasDeReplicas(_id_contenido, _id_comentario, _id_replica):
+
+@app.route("/contenido/<int:_id_contenido>/comentario/<int:id_comentario>/replica/<int:id_replica>",methods=['GET'])
+def obtenerReplicasDeReplicas(_id_contenido, id_comentario, id_replica):
     # replicas = db.session.execute(f"""SELECT t.apodo,t.descripcion,t.id_comentario,t.id
     #                 FROM replica as t , tiene as x
     #                 WHERE t.id = x.id_replica_siguiente AND x.id_replica_actual={_id_replica} """)
-    replicas = Replica.query.filter_by(id=_id_replica).first()
-    return render_template('replicasDeReplicas.html', replicas=replicas.rel, id_comentario=_id_comentario, id_contenido = _id_contenido, id=_id_replica)
+    replicas = Replica.query.filter_by(id=id_replica).first()
+    return render_template('replicasDeReplicas.html', replicas=replicas.rel, id_comentario=id_comentario, id_contenido = _id_contenido, id=id_replica)
 
 
 #genera replicas :)
